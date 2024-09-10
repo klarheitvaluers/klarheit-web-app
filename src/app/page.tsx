b1266cff3d8e467667b3aef9c41f6b473a61ebb1
sinster2003 <sindhurvshabaraya2318@gmail.com>
2024-09-11 01:36:11 +0530

    config fonts and theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+import ToggleMode from "@/components/ToggleMode";
 
 export default function Home() {
   return (
-    <div className="">
-      Home
+    <div>
+      {/* just for testing configuration */}
+      <div className="h-screen p-5">
+        <div className="flex flex-row items-center justify-between">
+          <div className="flex flex-col gap-2">
+            <h1 className="font-mona font-bold text-5xl">
+              Klarheit
+            </h1>
+            <p className="font-mona font-normal text-lg">
+              Welcome to KlarheitValuers
+            </p>
+          </div>
+          <ToggleMode/>
+        </div>
+      </div>
     </div>
   );
 }
