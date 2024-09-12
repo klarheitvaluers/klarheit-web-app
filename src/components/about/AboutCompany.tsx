60c321bd181e5bd1e3f71a7839acad55806ab283
sinster2003 <sindhurvshabaraya2318@gmail.com>
2024-09-13 03:03:38 +0530

    add why-us component and testimonial improvements

--- src/components/about/AboutCompany.tsx
+++ src/components/about/AboutCompany.tsx
@@ -1,7 +1,10 @@
+import WhyUs from "./WhyUs"
 
 const AboutCompany = () => {
   return (
-    <div>AboutCompany</div>
+    <div className="w-[90%] h-[90vh] m-auto my-10 rounded-3xl shadow-surround dark:shadow-none p-[60px]">
+      <WhyUs/>
+    </div>
   )
 }
 
