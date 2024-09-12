aee8ffd25218ba55cc95f426b75838fd54c7be49
sinster2003 <sindhurvshabaraya2318@gmail.com>
2024-09-12 17:22:39 +0530

    add-global-font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
       <div className="h-screen p-5">
         <div className="flex flex-row items-center justify-between">
           <div className="flex flex-col gap-2">
-            <h1 className="font-mona font-bold text-5xl">
+            <h1 className="font-bold text-5xl">
               Klarheit
             </h1>
-            <p className="font-mona font-normal text-lg">
+            <p className="font-normal text-lg">
               Welcome to KlarheitValuers
             </p>
           </div>
