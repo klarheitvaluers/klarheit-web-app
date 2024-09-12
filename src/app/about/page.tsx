549438caf3f38451da5f2f80e2c76522888ce220
sinster2003 <sindhurvshabaraya2318@gmail.com>
2024-09-12 23:05:29 +0530

    add testimonial cards

---
+++ src/app/about/page.tsx
@@ -0,0 +1,19 @@
+import AboutCompany from "@/components/about/AboutCompany";
+import Awards from "@/components/about/Awards";
+import Events from "@/components/about/Events";
+import Team from "@/components/about/Team";
+import Testimonials from "@/components/about/Testimonials";
+
+const About = () => {
+    return (
+        <div className="flex flex-col gap-6">
+            <AboutCompany/>
+            <Testimonials/>
+            <Team/>
+            <Events/>
+            <Awards/>
+        </div>
+    )
+}
+
+export default About;
