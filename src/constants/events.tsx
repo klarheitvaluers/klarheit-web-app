e0d8e0a8c51349d171f2b4e6bd3790f30a3984c4
sinster2003 <sindhurvshabaraya2318@gmail.com>
2024-09-14 02:18:28 +0530

    add events section

---
+++ src/constants/events.tsx
@@ -0,0 +1,22 @@
+import Avatar from "@/public/avatar.jpg";
+
+export const events = [
+    {
+        id: 1,
+        image: Avatar,
+        title: "AI & Blockchain in Real Estate Valuation",
+        description: "Explore how AI and blockchain are transforming property valuation processes. Learn about automated valuation models and blockchain-enabled transparency, with workshops and demos for integrating these technologies into valuation workflows."
+    },
+    {
+        id: 2,
+        image: Avatar,
+        title: "Sustainable Real Estate Development and Valuation",
+        description: "Discover how green buildings, renewable energy, and eco-friendly designs impact property values. Panel discussions with experts and workshops on integrating sustainability into valuation models will be held."
+    },
+    {
+        id: 3,
+        image: Avatar,
+        title: "Smart Cities: The Future of Urban Valuation",
+        description: "Focuses on how smart infrastructure, IoT, and big data are shaping property valuations in smart cities. Case studies and keynote talks from experts on how technology-driven cities are changing appraisal standards."
+    }
+]
