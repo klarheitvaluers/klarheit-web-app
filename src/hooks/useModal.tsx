f0e62b90cebe433c492ff96528052b305b1b6bf4
sinster2003 <sindhurvshabaraya2318@gmail.com>
2024-09-21 21:03:48 +0530

    fix about deployment

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,8 +1,12 @@
 import awardModalAtom from "@/atoms/AwardModalAtom";
-import { useRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 
 const useModal = () => {
   return useRecoilState<boolean>(awardModalAtom);
+}
+
+export const useSetModal = () => {
+  return useSetRecoilState(awardModalAtom);
 }
 
 export default useModal;
