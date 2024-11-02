ef938ffc5e60f18acf0d444eed97232135a659fa
rakshithapatel08 <rakshithapatel0807@gmail.com>
2024-11-03 00:11:57 +0530

    features,services,process-services page

---
+++ src/app/services/page.tsx
@@ -0,0 +1,17 @@
+import Clients from "@/components/servicesComponents/Clients"
+import Features from "@/components/servicesComponents/Features"
+import Process from "@/components/servicesComponents/Process"
+import Services from "@/components/servicesComponents/Services"
+
+const ServicesPage = () => {
+  return (
+    <div>
+        <Process/>
+        <Features/>
+        <Services/>
+        <Clients/>
+    </div>
+  )
+}
+
+export default ServicesPage
