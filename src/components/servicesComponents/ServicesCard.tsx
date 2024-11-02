4d139273ec349d2e9a499f1c88bc39096d23e7ed
rakshithapatel08 <rakshithapatel0807@gmail.com>
2024-11-03 00:16:59 +0530

    features,services,process-services page build errors solved

--- src/components/servicesComponents/ServicesCard.tsx
+++ src/components/servicesComponents/ServicesCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const ServicesCard = ({icon,service}:{icon:React.ReactNode,service:String}) => {
+const ServicesCard = ({icon,service}:{icon:React.ReactNode,service:string}) => {
   return (
     <div className='bg-grey-base w-56 h-32 rounded-[36px] flex justify-center items-center flex-col px-5'>
           <div className='text-white bg-orange rounded-full w-16 h-16 flex mb-32 absolute justify-center items-center'>{icon}</div>
