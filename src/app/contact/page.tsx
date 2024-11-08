6106d742acfe7b6bf443c0e65b3b54afaa701772
rakshithapatel08 <rakshithapatel0807@gmail.com>
2024-11-08 19:58:11 +0530

    contact page ui

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,12 @@
+import ContactCard from '@/components/contact/ContactCard'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <ContactCard/>
+    </div>
+  )
+}
+
+export default page
